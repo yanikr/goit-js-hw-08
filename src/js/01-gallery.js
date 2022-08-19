9a7edc0ed0c2e43c1a0f14603a006fbfe0f294b8
Yanik Rikhter <rihter.yan@gmail.com>
2022-08-19 15:42:54 +0300

    +player
    +++

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galSel = document.querySelector('.gallery');
+
+const imagesMap = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item gallery__link" href="${original}"><img class="gallery__image" src=${preview} data-source=${original} alt="${description}" title="${description}"></img></a>`
+  )
+  .join('');
+
+galSel.insertAdjacentHTML('beforeend', imagesMap);
+
+const gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+  gallery.defaultOptions.captionDelay = 250;
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+let time = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(time)
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
