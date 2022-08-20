cc046e5a450adc50dfbf6431a6e84b53fd1a13a3
Yanik Rikhter <rihter.yan@gmail.com>
2022-08-20 15:12:20 +0300

    feedback form done
    +++

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('form');
 
+let feedback = {};
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('input', throttle(handleInput, 500));
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+}
+
+function handleInput(event) {
+  feedback[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedback));
+}
